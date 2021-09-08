2fd9e6a2cb0974f55b40b1058ce584bae84aa02f
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2021-09-08 12:31:10 -0300

    228

--- script.js
+++ script.js
@@ -14,6 +14,12 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
+    //remover o mosquito anterior
+if(document.getElementById('mosquito')){
+    document.getElementById('mosquito').remove()
+}
+    
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -29,6 +35,7 @@ function posicaoRandomica() {
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id ='mosquito'
 
     document.body.appendChild(mosquito)
 
