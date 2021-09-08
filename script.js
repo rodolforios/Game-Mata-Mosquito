bae6d8cf52e3b00203ccbd7667360954676dea3e
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2021-09-08 12:15:04 -0300

    226

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var altura = 0
 var largura = 0
 
 function ajustaTamanhoPalcoJogo() {
-	altura = window.innerHeight
-	largura = window.innerWidth
+    altura = window.innerHeight
+    largura = window.innerWidth
 
-	console.log(largura, altura)
+    console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
@@ -14,21 +14,41 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
-	var posicaoX = Math.floor(Math.random() * largura) - 90
-	var posicaoY = Math.floor(Math.random() * altura) - 90
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
 
-	posicaoX = posicaoX < 0 ? 0 : posicaoX
-	posicaoY = posicaoY < 0 ? 0 : posicaoY
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
 
-	console.log(posicaoX, posicaoY)
+    console.log(posicaoX, posicaoY)
 
-	//criar o elemento html
-	var mosquito = document.createElement('img')
-	mosquito.src = "imagens/mosca.png"
-	mosquito.className = 'mosquito1'
-	mosquito.style.left = posicaoX + 'px'
-	mosquito.style.top = posicaoY + 'px'
-	mosquito.style.position = 'absolute'
+    //criar o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = "imagens/mosca.png"
+    mosquito.className = tamanhoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
 
-	document.body.appendChild(mosquito)
+    document.body.appendChild(mosquito)
+
+    
+    console.log(tamanhoAleatorio())
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+    
+
+    switch(classe) {
+
+        case 0:
+            return 'mosquito1'
+
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+            return 'mosquito3'
+    }
 }
